eed278e0df06d744de678f9cfefb5be4c21ab55d
Harry <phuothuynh2002@gmail.com>
2024-05-27 22:29:54 +0700

    update husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 import { Button } from '@nextui-org/button';
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <Button>
-          VFoody
-          </Button>
-        <h1 className="text-4xl font-bold text-center mt-8"
-        >
-          Welcome to VFoody
-                </h1>
+        <Button>VFoody </Button>
+        <h1 className="text-4xl font-bold text-center mt-8">Welcome to VFoody</h1>
       </div>
     </main>
   );
