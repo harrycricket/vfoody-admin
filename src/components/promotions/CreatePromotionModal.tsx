c263ec4a8e296b657e4154430d8aab3c4c2068fa
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-06-25 09:36:37 +0700

    [Promotion Page] Create Form UI & Validation & Sample Submit

--- src/components/promotions/CreatePromotionModal.tsx
+++ src/components/promotions/CreatePromotionModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import {
   Modal,
   ModalContent,
@@ -7,14 +7,11 @@ import {
   ModalFooter,
   Button,
   Input,
-  Checkbox,
   Select,
-  useDisclosure,
   SelectItem,
   Textarea,
-  Avatar,
-  Switch,
   Image,
+  Switch,
 } from '@nextui-org/react';
 import PromotionModel, {
   PromotionApplyType,
@@ -36,6 +33,7 @@ export default function CreatePromotionModal({
   onOpenChange,
   onClose,
 }: CreatePromotionModalProps) {
+  const isAnyRequestSubmit = useRef(false);
   const [promotion, setPromotion] = useState<PromotionModel>({
     id: 0,
     title: '',
@@ -48,56 +46,96 @@ export default function CreatePromotionModal({
     amountValue: 0,
     applyType: PromotionApplyType.RateApply,
     status: PromotionStatus.Active,
-    startDate: '',
-    endDate: '',
-    usageLimit: 0,
+    startDate: new Date().toDateString(),
+    endDate: new Date().toDateString(),
+    usageLimit: 100,
     numberOfUsed: 0,
     promotionType: '',
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const [errors, setErrors] = useState<any>({});
+  const validate = (promotion: PromotionModel) => {
+    let tempErrors: any = {};
+    if (promotion.title.length < 6) tempErrors.title = 'Tiêu đề ít nhất 6 kí tự.';
+    if (
+      promotion.applyType == PromotionApplyType.RateApply &&
+      (promotion.amountRate < 1 || promotion.amountRate > 100)
+    )
+      tempErrors.amountRate = 'Tỉ lệ giảm giá nằm trong khoảng từ 1 đến 100 (%).';
+    if (promotion.minimumOrderValue < 0)
+      tempErrors.minimumOrderValue = 'Giá trị đơn hàng tối thiểu lớn hơn hoặc bằng 0.';
+    if (promotion.maximumApplyValue < 0)
+      tempErrors.maximumApplyValue = 'Giá trị áp dụng tối đa cần lớn hơn hoặc bằng 0.';
+    if (promotion.applyType == PromotionApplyType.AmountApply && promotion.amountValue < 1000)
+      tempErrors.amountValue = 'Giá trị giảm giá cần lớn hơn hoặc bằng 1000 đồng.';
+    if (new Date(promotion.startDate) > new Date(promotion.endDate))
+      tempErrors.startDate = 'Ngày bắt đầu cần trước hoặc bằng ngày kết thúc';
+    if (promotion.usageLimit < 0)
+      tempErrors.usageLimit = 'Giới hạn lượt sử dụng lớn hơn hoặc bằng 0.';
+    setErrors(tempErrors);
+    return Object.keys(tempErrors).length === 0;
+  };
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>,
+  ) => {
     const { name, value } = e.target;
+    if (isAnyRequestSubmit.current) {
+      validate({
+        ...promotion,
+        [name]: value,
+      });
+    }
     setPromotion((prevPromotion) => ({
       ...prevPromotion,
       [name]: value,
     }));
   };
 
-  const handleSave = () => {
-    console.log('Promotion details:', promotion);
-    onClose();
+  const handleSubmit = () => {
+    isAnyRequestSubmit.current = true;
+    console.log('Promotion details:', promotion, validate(promotion), errors);
+    if (validate(promotion)) {
+      console.log('Promotion details:', promotion);
+      onClose();
+    }
   };
 
   return (
-    <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
-      <ModalContent style={{ width: '1000px' }}>
+    <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center" size="4xl">
+      <ModalContent>
         {(onClose) => (
           <>
             <ModalHeader className="flex flex-col gap-1">
               Tạo mới chương trình khuyến mãi
             </ModalHeader>
             <ModalBody>
-              <div className="flex gap-1">
-                <div className="flex flex-col gap-1">
-                  <Input
-                    name="title"
-                    label="Tiêu đề"
-                    placeholder="Nhập tiêu đề chương trình khuyến mãi"
-                    value={promotion.title}
-                    onChange={handleChange}
-                    fullWidth
-                  />
-                  <Textarea
-                    name="description"
-                    label="Mô tả"
-                    placeholder="Nhập mô tả chương trình khuyến mãi"
-                    value={promotion.description}
-                    onChange={handleChange}
-                    fullWidth
-                  />
+              <div className="flex gap-3">
+                <div className="flex-1 flex flex-col gap-2">
+                  <div className="input-container">
+                    <Input
+                      name="title"
+                      label="Tiêu đề"
+                      placeholder="Nhập tiêu đề chương trình khuyến mãi"
+                      value={promotion.title}
+                      onChange={handleChange}
+                      isInvalid={errors.title ? true : false}
+                      errorMessage={errors.title}
+                      fullWidth
+                    />
+                  </div>
+                  <div className="input-container">
+                    <Textarea
+                      name="description"
+                      label="Mô tả"
+                      placeholder="Nhập mô tả chương trình khuyến mãi"
+                      value={promotion.description}
+                      onChange={handleChange}
+                      fullWidth
+                    />
+                  </div>
                   <Image width={'100%'} radius="md" src={promotion.bannerUrl} />
                 </div>
-                <div className="flex flex-col gap-1">
+                <div className="flex-1 flex flex-col justify-between">
                   <div className="flex gap-1">
                     <Input
                       name="startDate"
@@ -106,6 +144,7 @@ export default function CreatePromotionModal({
                       value={promotion.startDate}
                       onChange={handleChange}
                       fullWidth
+                      isInvalid={errors.startDate ? true : false}
                     />
                     <Input
                       name="endDate"
@@ -114,93 +153,128 @@ export default function CreatePromotionModal({
                       value={promotion.endDate}
                       onChange={handleChange}
                       fullWidth
+                      isInvalid={errors.startDate ? true : false}
                     />
                   </div>
-
-                  <Select
-                    name="applyType"
-                    label="Loại áp dụng"
-                    selectedKeys={new Set([promotion.applyType.toString()])}
-                    onChange={handleChange}
-                    fullWidth
-                  >
-                    {promotionApplyTypes.map((type) => (
-                      <SelectItem key={type.key} value={type.key}>
-                        {type.label}
-                      </SelectItem>
-                    ))}
-                  </Select>
-                  {promotion.applyType == PromotionApplyType.RateApply ? (
-                    <Input
-                      name="amountRate"
-                      label="Tỷ lệ giảm giá (%)"
-                      placeholder="Nhập tỷ lệ giảm giá"
-                      type="number"
-                      value={promotion.amountRate.toString()}
-                      onChange={handleChange}
-                      fullWidth
-                    />
-                  ) : (
-                    <Input
-                      name="amountValue"
-                      label="Giá trị giảm giá (VND)"
-                      placeholder="Nhập giá trị giảm giá"
-                      type="number"
-                      value={promotion.amountValue.toString()}
-                      onChange={handleChange}
-                      fullWidth
-                    />
+                  {errors.startDate && (
+                    <span className="text-tiny text-danger">{errors.startDate}</span>
                   )}
 
-                  <Input
-                    name="minimumOrderValue"
-                    label="Đơn hàng tối thiểu"
-                    placeholder="Nhập giá trị đơn hàng tối thiểu"
-                    type="number"
-                    value={promotion.minimumOrderValue.toString()}
-                    onChange={handleChange}
-                    fullWidth
-                  />
-                  <Input
-                    name="maximumApplyValue"
-                    label="Giá trị tối đa"
-                    placeholder="Nhập giá trị tối đa"
-                    type="number"
-                    value={promotion.maximumApplyValue.toString()}
-                    onChange={handleChange}
-                    fullWidth
-                  />
-                  <Input
-                    name="usageLimit"
-                    label="Giới hạn số lần sử dụng"
-                    placeholder="Nhập số lần sử dụng tối đa"
-                    type="number"
-                    value={promotion.usageLimit.toString()}
-                    onChange={handleChange}
-                    fullWidth
-                  />
+                  <div className="input-container">
+                    <Select
+                      name="applyType"
+                      label="Loại áp dụng"
+                      selectedKeys={new Set([promotion.applyType.toString()])}
+                      onChange={handleChange}
+                      fullWidth
+                    >
+                      {promotionApplyTypes.map((type) => (
+                        <SelectItem key={type.key} value={type.key}>
+                          {type.label}
+                        </SelectItem>
+                      ))}
+                    </Select>
+                  </div>
+                  {promotion.applyType == PromotionApplyType.RateApply ? (
+                    <div className="input-container">
+                      <Input
+                        name="amountRate"
+                        label="Tỷ lệ giảm giá (%)"
+                        placeholder="Nhập tỷ lệ giảm giá"
+                        type="number"
+                        value={promotion.amountRate.toString()}
+                        onChange={handleChange}
+                        fullWidth
+                        isInvalid={errors.amountRate ? true : false}
+                        errorMessage={errors.amountRate}
+                      />
+                    </div>
+                  ) : (
+                    <div className="input-container">
+                      <Input
+                        name="amountValue"
+                        label="Giá trị giảm giá (VND)"
+                        placeholder="Nhập giá trị giảm giá"
+                        type="number"
+                        value={promotion.amountValue.toString()}
+                        onChange={handleChange}
+                        fullWidth
+                        isInvalid={errors.amountValue ? true : false}
+                        errorMessage={errors.amountValue}
+                      />
+                    </div>
+                  )}
+
+                  <div className="input-container">
+                    <Input
+                      name="minimumOrderValue"
+                      label="Đơn hàng tối thiểu"
+                      placeholder="Nhập giá trị đơn hàng tối thiểu"
+                      type="number"
+                      value={promotion.minimumOrderValue.toString()}
+                      onChange={handleChange}
+                      fullWidth
+                      isInvalid={errors.minimumOrderValue ? true : false}
+                      errorMessage={errors.minimumOrderValue}
+                    />
+                  </div>
+                  <div className="input-container">
+                    <Input
+                      name="maximumApplyValue"
+                      label="Giá trị tối đa"
+                      placeholder="Nhập giá trị tối đa"
+                      type="number"
+                      value={
+                        promotion.applyType == PromotionApplyType.AmountApply
+                          ? promotion.amountValue.toString()
+                          : promotion.maximumApplyValue.toString()
+                      }
+                      readOnly={promotion.applyType == PromotionApplyType.AmountApply}
+                      style={{
+                        opacity: promotion.applyType === PromotionApplyType.AmountApply ? 0.7 : 1,
+                      }}
+                      onChange={handleChange}
+                      fullWidth
+                      isInvalid={errors.maximumApplyValue ? true : false}
+                      errorMessage={errors.maximumApplyValue}
+                    />
+                  </div>
+                  <div className="input-container">
+                    <Input
+                      name="usageLimit"
+                      label="Giới hạn lượt sử dụng"
+                      placeholder="Nhập số lần sử dụng tối đa"
+                      type="number"
+                      value={promotion.usageLimit.toString()}
+                      onChange={handleChange}
+                      fullWidth
+                      isInvalid={errors.usageLimit ? true : false}
+                      errorMessage={errors.usageLimit}
+                    />
+                  </div>
                 </div>
               </div>
-
-              <Switch
-                name="status"
-                checked={promotion.status === PromotionStatus.Active}
-                onChange={(checked) =>
-                  setPromotion((prevPromotion) => ({
-                    ...prevPromotion,
-                    status: checked ? PromotionStatus.Active : PromotionStatus.UnActive,
-                  }))
-                }
-              >
-                {promotion.status === PromotionStatus.Active ? 'Bật hoạt động' : 'Tạm thời tắt'}
-              </Switch>
+              <div className="flex justify-end p-2">
+                <Switch
+                  name="status"
+                  isSelected={promotion.status == PromotionStatus.Active}
+                  onValueChange={(checked) => {
+                    setPromotion((prevPromotion) => ({
+                      ...prevPromotion,
+                      status: checked ? PromotionStatus.Active : PromotionStatus.UnActive,
+                    }));
+                  }}
+                >
+                  Trạng thái hoạt động
+                </Switch>
+              </div>
             </ModalBody>
             <ModalFooter>
+              <Button color="primary" onPress={handleSubmit}>
+                Lưu
+              </Button>
               <Button color="danger" variant="flat" onPress={onClose}>
                 Đóng
-              </Button>
-              <Button color="primary" onPress={handleSave}>
-                Lưu
               </Button>
             </ModalFooter>
           </>
