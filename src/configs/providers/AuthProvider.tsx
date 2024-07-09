fd31d9922ce410dec7c63999284631dd87357e45
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-07-09 10:47:24 +0700

    [Admin Page] Update auth provider

--- src/configs/providers/AuthProvider.tsx
+++ src/configs/providers/AuthProvider.tsx
@@ -1,16 +1,27 @@
 'use client';
+import apiClient from '@/services/api-services/api-client';
+import APICommonResponse from '@/types/responses/APICommonResponse';
 import { useRouter, usePathname } from 'next/navigation';
 import React, { ReactNode, useEffect } from 'react';
 
 const AuthProvider = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
   const pathname = usePathname();
-  const isAuthenticated = () => {
-    const token = localStorage.getItem('token');
-    return token !== null;
+  const isAuthenticated = async () => {
+    const token = localStorage.getItem('token') || '';
+    let result = true;
+    await apiClient
+      .get<APICommonResponse>('auth/admin')
+      .then((response) => {
+        result = true;
+      })
+      .catch((error) => {
+        result = false;
+      });
+    return result;
   };
 
-  if (!pathname.includes('login') && !isAuthenticated()) {
+  if (typeof window !== 'undefined' && !pathname.includes('login') && !isAuthenticated()) {
     router.push('/login');
   }
 
