da5fb4e7b2594963d30bc3cfa6cceb117eaab109
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-07-12 22:27:48 +0700

    [Dashboard] Admin Dashboard Update

--- src/components/dashboard/DashboardOverview.tsx
+++ src/components/dashboard/DashboardOverview.tsx
@@ -6,7 +6,7 @@ import APICommonResponse from '@/types/responses/APICommonResponse';
 import { DashboardOverviewAPIReponse } from '@/types/responses/DashboardResponse';
 import { Skeleton } from '@nextui-org/react';
 import React from 'react';
-import { FaArrowUpLong, FaMoneyBillWheat, FaUser } from 'react-icons/fa6';
+import { FaArrowDownLong, FaArrowUpLong, FaMoneyBillWheat, FaUser } from 'react-icons/fa6';
 import { FcMoneyTransfer } from 'react-icons/fc';
 import { GiBuyCard } from 'react-icons/gi';
 import numberFormatUtilServicevice from '@/services/util-services/NumberFormatUtilService';
@@ -27,10 +27,25 @@ const DashboardOverview = () => {
   );
 
   const totalTradingRate = data ? Math.round(data.value.totalTradingRate) : 0;
+  const totalTrading = numberFormatUtilServicevice.formatNumberWithDotEach3digits(
+    data ? Math.round(data.value.totalTrading) : 0,
+  );
   const totalRevenueRate = data ? Math.round(data.value.totalRevenueRate) : 0;
+  const totalRevenue = numberFormatUtilServicevice.formatNumberWithDotEach3digits(
+    data ? Math.round(data.value.totalRevenueRate) : 0,
+  );
   const totalOrderRate = data ? Math.round(data.value.totalOrderRate) : 0;
+  const totalOrder = numberFormatUtilServicevice.formatNumberWithDotEach3digits(
+    data ? Math.round(data.value.totalOrder) : 0,
+  );
   const totalUserRate = data ? Math.round(data.value.totalUserRate) : 0;
-
+  const totalUser = numberFormatUtilServicevice.formatNumberWithDotEach3digits(
+    data ? Math.round(data.value.totalUser) : 0,
+  );
+  console.log(
+    data?.value?.totalTrading,
+    numberFormatUtilServicevice.formatNumberWithDotEach3digits(data?.value?.totalTrading || 0),
+  );
   return (
     <div className="grid grid-cols-2 gap-4 w-full">
       <div className="bg-white p-6 rounded-lg shadow-md flex items-center gap-4">
@@ -43,19 +58,18 @@ const DashboardOverview = () => {
             {isLoading && data ? (
               <Skeleton className="flex rounded-full w-20 h-8" />
             ) : data?.value.totalTrading ? (
-              numberFormatUtilServicevice.formatThousandNumberWithDotEach3digits(
-                data.value.totalTrading,
-              ) + ' đ'
+              totalTrading + ' đ'
             ) : (
               '---'
             )}
           </p>
           <div className="flex items-center mt-[4px]">
             <div className="bg-green-100 flex justify-center items-center w-[16px] h-[16px] rounded-full">
-              <FaArrowUpLong
-                color={`${data && data?.value.totalTradingRate < 0 ? 'red' : 'green'}`}
-                size={8}
-              />
+              {data && data?.value.totalTradingRate < 0 ? (
+                <FaArrowDownLong color={'red'} size={8} />
+              ) : (
+                <FaArrowUpLong color={'green'} size={8} />
+              )}
             </div>
             <span
               className={`text-${data && data?.value.totalTradingRate < 0 ? 'red' : 'green'}-500 text-sm ml-2 `}
@@ -79,19 +93,18 @@ const DashboardOverview = () => {
             {isLoading && data ? (
               <Skeleton className="flex rounded-full w-20 h-8" />
             ) : data?.value.totalRevenue ? (
-              numberFormatUtilServicevice.formatThousandNumberWithDotEach3digits(
-                data.value.totalRevenue,
-              ) + ' đ'
+              totalRevenue + ' đ'
             ) : (
               '---'
             )}
           </p>
           <div className="flex items-center mt-[4px]">
             <div className="bg-green-100 flex justify-center items-center w-[16px] h-[16px] rounded-full">
-              <FaArrowUpLong
-                color={`${data && data?.value.totalRevenueRate < 0 ? 'red' : 'green'}`}
-                size={8}
-              />
+              {data && data?.value.totalRevenueRate < 0 ? (
+                <FaArrowDownLong color={'red'} size={8} />
+              ) : (
+                <FaArrowUpLong color={'green'} size={8} />
+              )}
             </div>
             <span
               className={`text-${data && data?.value.totalRevenueRate < 0 ? 'red' : 'green'}-500 text-sm ml-2 `}
@@ -115,17 +128,18 @@ const DashboardOverview = () => {
             {isLoading && data ? (
               <Skeleton className="flex rounded-full w-20 h-8" />
             ) : data?.value.totalOrder ? (
-              numberFormatUtilServicevice.formatNumberWithDotEach3digits(data.value.totalOrder)
+              totalOrder
             ) : (
               '---'
             )}
           </p>
           <div className="flex items-center mt-[4px]">
             <div className="bg-green-100 flex justify-center items-center w-[16px] h-[16px] rounded-full">
-              <FaArrowUpLong
-                color={`${data && data?.value.totalOrderRate < 0 ? 'red' : 'green'}`}
-                size={8}
-              />
+              {data && data?.value.totalOrderRate < 0 ? (
+                <FaArrowDownLong color={'red'} size={8} />
+              ) : (
+                <FaArrowUpLong color={'green'} size={8} />
+              )}
             </div>
             <span
               className={`text-${data && data?.value.totalOrderRate < 0 ? 'red' : 'green'}-500 text-sm ml-2 `}
@@ -150,17 +164,18 @@ const DashboardOverview = () => {
             {isLoading && data ? (
               <Skeleton className="flex rounded-full w-20 h-8" />
             ) : data?.value.totalUser ? (
-              numberFormatUtilServicevice.formatNumberWithDotEach3digits(data.value.totalUser)
+              totalUser
             ) : (
               '---'
             )}
           </p>
           <div className="flex items-center mt-[4px]">
             <div className="bg-green-100 flex justify-center items-center w-[16px] h-[16px] rounded-full">
-              <FaArrowUpLong
-                color={`${data && data?.value.totalUserRate < 0 ? 'red' : 'green'}`}
-                size={8}
-              />
+              {data && data?.value.totalUserRate < 0 ? (
+                <FaArrowDownLong color={'red'} size={8} />
+              ) : (
+                <FaArrowUpLong color={'green'} size={8} />
+              )}
             </div>
             <span
               className={`text-${data && data?.value.totalUserRate < 0 ? 'red' : 'green'}-500 text-sm ml-2 `}
