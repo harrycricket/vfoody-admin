9fcf5f37857b1e6bb526837fb4c7bdf4680ef311
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-07-30 01:10:21 +0700

    [Deploy] Add Grading Mail Notification

--- src/configs/providers/AuthProvider.tsx
+++ src/configs/providers/AuthProvider.tsx
@@ -30,7 +30,7 @@ const AuthProvider = ({ children }: { children: ReactNode }) => {
   };
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== 'undefined' || pathname.includes('mln122-grading-mail-noti')) {
       authenticate();
     }
   }, []);
